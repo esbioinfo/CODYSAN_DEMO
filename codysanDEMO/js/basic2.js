f4797338d6eeddc942ebaafcb9f0d89ec71d8f65
Edgar <43356362+esbioinfo@users.noreply.github.com>
2021-06-17 15:22:00 +0200

    Updated version,
    This is the last version I released for CODYSAN during my Final Master's Degree Project.
    
    You can check the demo live here:
    https://esbioinfo.mindtheocean.com/codysanDEMO/

---
+++ codysanDEMO/js/basic2.js
@@ -0,0 +1,42 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+	"popup": {
+	  "background": "#237afc"
+	},
+	"button": {
+	  "background": "#fff",
+	  "text": "#237afc"
+	}
+  },
+  "theme": "classic",
+  "position": "bottom-left",
+  "content": {
+	"href": "cookies.html"
+  }
+})});
+
+function getBrowserLanguage(){
+	// First, check the session storage to see if we have the language defined.
+	if (sessionStorage['userLang']) {
+		//alert("There is 'userLang' in session storage. It's set to " + sessionStorage.getItem("userLang"))
+		var userLang = sessionStorage.getItem("userLang");
+	} else {
+		// Language detection
+		var userLang = navigator.language || navigator.userLanguage; 
+		if (userLang == "ca" || userLang == "ca-ES") {
+			userLang = "ca";
+		} else if (userLang == "es" || userLang == "es-ES") {
+			userLang = "es";
+		} else {
+			userLang = "en";
+		}
+		//localStorage.setItem("userLang", userLang);
+		//alert("The language is set to: " + userLang);
+	}
+	// HTML5 session Storage
+	sessionStorage.setItem("userLang",userLang);
+};
+
+
+
